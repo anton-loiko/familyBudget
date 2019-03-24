98080cc1270f20d452361331219c35629b254756
George <george_tr@quantstamp.com>
2019-03-24 13:28:42 +0200

    Update: fix disable btn (no-multy-click)

--- app/components/AddButton/index.js
+++ app/components/AddButton/index.js
@@ -3,11 +3,23 @@ import { TouchableWithoutFeedback, View, Animated } from 'react-native'
 import { styles, BTN_SIZE } from './style'
 
 const toBottomValue = 8
+let isDisable = 0
 
-export default ({ onFirstPress, onSecondPress }) => {
+const funcDef = () => {}
+
+export default ({ onFirstPress = funcDef, onSecondPress = funcDef }) => {
   const [isOpen, setOpen] = useState(false)
 
-  let isDisable
+  useEffect(() => {
+    if (isOpen) {
+      isDisable = false
+    }
+    if (!isOpen) {
+      isDisable = false
+    }
+    return clearTimeouts()
+  }, [isOpen])
+
   let animateTimeout
   let clickFirstTimeout
   let clickSecondTimeout
@@ -17,10 +29,6 @@ export default ({ onFirstPress, onSecondPress }) => {
     clearTimeout(clickFirstTimeout)
     clearTimeout(clickSecondTimeout)
   }
-
-  useEffect(() => {
-    return clearTimeouts()
-  }, [])
 
   const top = new Animated.Value(-(BTN_SIZE / 2))
   const rotateValue = new Animated.Value(0)
@@ -34,62 +42,74 @@ export default ({ onFirstPress, onSecondPress }) => {
     transform: [{ rotate: interpolateRotation }],
   }
 
-  const startAnimation = () => {
-    isDisable = true
+  const firstAnimation = () => {
+    if (isDisable) return
 
+    const duration = 500
+
+    Animated.timing(top, { toValue: toBottomValue, friction: 0.8 }).start()
+    Animated.timing(rotateValue, {
+      toValue: 1,
+      duration,
+    }).start()
+
+    clickFirstTimeout = setTimeout(() => {
+      onFirstPress()
+      isDisable = 0
+      setOpen(true)
+    }, duration)
+  }
+
+  const secondAnimation = () => {
+    if (isDisable) return
+
+    const duration = 1200
+    top.setValue(toBottomValue)
+    Animated.timing(top, {
+      toValue: -(BTN_SIZE * 1.5),
+      friction: 0.8,
+    }).start()
+
+    animateTimeout = setTimeout(() => {
+      Animated.parallel([
+        Animated.timing(top, {
+          toValue: -(BTN_SIZE / 2),
+          friction: 0.5,
+        }),
+        Animated.timing(rotateValue, {
+          toValue: 3,
+          duration: 500,
+        }),
+      ]).start()
+    }, 500)
+
+    Animated.timing(rotateValue, {
+      toValue: 2,
+      duration,
+    }).start()
+
+    clickSecondTimeout = setTimeout(() => {
+      onSecondPress()
+      isDisable = 0
+      setOpen(false)
+    }, 1000)
+  }
+
+  const startAnimation = async () => {
     if (isOpen) {
-      const duration = 1200
-      top.setValue(toBottomValue)
-      Animated.timing(top, {
-        toValue: -(BTN_SIZE * 1.5),
-        friction: 0.8,
-      }).start()
-
-      animateTimeout = setTimeout(() => {
-        Animated.parallel([
-          Animated.timing(top, {
-            toValue: -(BTN_SIZE / 2),
-            friction: 0.5,
-          }),
-          Animated.timing(rotateValue, {
-            toValue: 3,
-            duration: 500,
-          }),
-        ]).start()
-      }, 500)
-
-      Animated.timing(rotateValue, {
-        toValue: 2,
-        duration,
-      }).start()
-
-      clickSecondTimeout = setTimeout(() => {
-        onSecondPress()
-        isDisable = false
-        setOpen(false)
-      }, 1000)
+      secondAnimation()
+      isDisable += 1
     }
 
     if (!isOpen) {
-      const duration = 500
-
-      Animated.timing(top, { toValue: toBottomValue, friction: 0.8 }).start()
-      Animated.timing(rotateValue, {
-        toValue: 1,
-        duration,
-      }).start()
-
-      clickFirstTimeout = setTimeout(() => {
-        onFirstPress()
-        isDisable = false
-        setOpen(true)
-      }, duration)
+      firstAnimation()
+      isDisable += 1
     }
   }
 
   return (
     <TouchableWithoutFeedback
-      disabled={isDisable}
+      disabled={!!isDisable}
       onPress={startAnimation}
       accessibilityLabel="Add new note"
       style={{ position: 'absolute', width: BTN_SIZE, height: BTN_SIZE }}
